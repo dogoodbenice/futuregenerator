f18ee5f49c1cb6e0075198fe848d8d8fcd6759cb
Suraj Rai <surajrajrai@gmail.com>
2021-07-13 21:19:40 +0100

    sketch

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -6,12 +6,16 @@ function preload() {
 }
 
 function setup() {
-  cnv = createCanvas(500, 500, WEBGL);
+  cnv = createCanvas(1500, 1500, WEBGL);
   centerCanvas();
   // Move the canvas so it’s inside our <div id="sketch-holder">.
   cnv.parent('sketch-holder');
   textFont(myFont);
   predictionchooser();
+}
+
+function draw() {
+
 }
 
 function centerCanvas() {
@@ -34,84 +38,89 @@ function mousePressed() {
 
 //In the future there will be
 function ITFTWB() {
-  translate(-240, -100, 0);
+  //translate(-240, -100, 0);
   textFont(myFont);
   text('We will use bananas as cars', 0, 0);
 }
 
 //In the future word
 function ITFTW() {
-  translate(-240, -100, 0);
+  //translate(-240, -100, 0);
   textFont(myFont);
   text('There will be a word addfedf to mean love', 0, 0);
 }
 
 //In the future word
 function CCV() {
+
+  push()
   translate(-240, -100, 0);
   textFont(myFont);
-  text('One bitcoin will be worth 5000', 0, 0);
+  text('One bitcoin will be worth'+random(0,50000)+' pounds', 0, 0);
+  pop()
 }
 
 
 function predictionchooser() {
- background(30);
-//backgroundanimation()
+ background('grey');
+ backgroundanimation()
   let r = random(5);
 
   if (r < 1) {
-    ITFTW()
+    CCV()
   } else if (r < 2) {
-    ITFTWB()
+    CCV()
   } else if (r < 3) {
     CCV()
   } else if (r < 4) {
-
+    CCV()
   } else {
-
+    CCV()
   }
 
 }
 
-// function backgroundanimation() {
-//   translate(-240, -100, 0);
-//   normalMaterial();
-//   push();
-//   rotateZ(frameCount * 0.01);
-//   rotateX(frameCount * 0.01);
-//   rotateY(frameCount * 0.01);
-//   plane(70);
-//   pop();
-//
-//   translate(240, 0, 0);
-//   push();
-//   rotateZ(frameCount * 0.01);
-//   rotateX(frameCount * 0.01);
-//   rotateY(frameCount * 0.01);
-//   box(70, 70, 70);
-//   pop();
-//
-//   translate(240, 0, 0);
-//   push();
-//   rotateZ(frameCount * 0.01);
-//   rotateX(frameCount * 0.01);
-//   rotateY(frameCount * 0.01);
-//   cylinder(70, 70);
-//   pop();
-//
-//   translate(-240 * 2, 200, 0);
-//   push();
-//   rotateZ(frameCount * 0.01);
-//   rotateX(frameCount * 0.01);
-//   rotateY(frameCount * 0.01);
-//   cone(70, 70);
-//   pop();
-//
-//   translate(240, 0, 0);
-//   push();
-//   rotateZ(frameCount * 0.01);
-//   rotateX(frameCount * 0.01);
-//   rotateY(frameCount * 0.01);
-//   torus(70, 20);
-//   pop();
-// }
+function backgroundanimation() {
+  push()
+  translate(-240, -100, 0);
+  normalMaterial();
+  push();
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  plane(70);
+  pop();
+
+  translate(240, 0, 0);
+  push();
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  box(70, 70, 70);
+  pop();
+
+  translate(240, 0, 0);
+  push();
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  cylinder(70, 70);
+  pop();
+
+  translate(-240 * 2, 200, 0);
+  push();
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  cone(70, 70);
+  pop();
+
+  translate(240, 0, 0);
+  push();
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  torus(70, 20);
+  pop();
+  pop();
+}
