03dfa0d4c29a9ac8c4c4a4d430656609b8449f62
Suraj Rai <surajrajrai@gmail.com>
2021-07-15 19:26:40 +0100

    sketch

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,9 +1,14 @@
 var cnv;
 var prediction
-let myFont, fbutton, sbutton;
+let myFont, fbutton, sbutton, cryptocurrencies;
+
+let pluralobjects = ["scarves", "ties", "watches", "gloves", "handbags", "laptops", "books", "pens", "pans", "pants", "phones", "chips", "comedians", "fishes", "cubes", "eggs", "aubergines", "light bulbs", "ladders", "pizzas", "sandwiches", "kebabs", "baked potatoes", "samosas", "biscuits", "apples", "avocadoes", "milkshakes", "cabbages", "carrots", "potatoes", "onions", "lemons", "bananas", "grapes", "peaches", "oranges", "boots", "slippers", "socks", "flip flops", "guitars", "pianos", "chickens", "ovens", "plants", "ballerinas", "pots"];
+
 
 function preload() {
   myFont = loadFont('assets/Aileron-BlackItalic.otf');
+  //pluralobjects = loadJSON("pluralobjects.json");
+  //cryptocurrencies = loadJSON("cryptocurrencies.json");
 }
 
 function setup() {
@@ -20,13 +25,13 @@ function setup() {
 
 function showmethefuturebutton() {
   fbutton = createButton('SHOW ME THE FUTURE');
-  fbutton.position((windowWidth) / 2.25, ((windowHeight) / 1.5)+50);
+  fbutton.position((windowWidth) / 2.25, ((windowHeight) / 1.5) + 50);
   fbutton.mousePressed(setup);
 }
 
 function saveprediction() {
   sbutton = createButton('SAVE MY PREDICTION');
-  sbutton.position((windowWidth) / 2.25, ((windowHeight) / 1.5)+100);
+  sbutton.position((windowWidth) / 2.25, ((windowHeight) / 1.5) + 100);
   //sbutton.mousePressed(saveCanvas('thefuture', 'jpg'));
 }
 
@@ -71,17 +76,26 @@ function CCV() {
   pop()
 }
 
+//In the future word
+function futureobjects() {
+  push()
+  translate(-240, (windowHeight - height) / 2, 0);
+  textFont(myFont);
+  prediction = text('In the future we will use ' + pluralobjects[Math.floor(Math.random() * pluralobjects.length)] + ' as ' + pluralobjects[Math.floor(Math.random() * pluralobjects.length)], 0, 0);
+  pop()
+}
+
 function predictionchooser() {
   background('black')
   let r = random(5);
   if (r < 1) {
     CCV()
   } else if (r < 2) {
-    ITFTWB()
+    futureobjects()
   } else if (r < 3) {
-    CCV()
+    futureobjects()
   } else if (r < 4) {
-    ITFTW()
+    futureobjects()
   } else {
     CCV()
   }
@@ -92,51 +106,44 @@ function backgroundshapes() {
   for (var i = 0; i < 3; i++) {
     normalMaterial();
     push()
-      translate(random(-500,500), random(-200,200), random(-100,100));
-      rotateZ(random(1,360) * 0.01);
-      rotateX(random(1,360) * 0.01);
-      rotateY(random(1,360) * 0.01);
-      torus(random(10,100), 20);
-    pop();
-    push();
-    translate(random(-500,500), random(-200,200), random(-100,100));
-    rotateZ(random(1,360) * 0.01);
-    rotateX(random(1,360) * 0.01);
-    rotateY(random(1,360) * 0.01);
-    plane(random(10,70));
+    translate(random(-500, 500), random(-200, 200), random(-100, 100));
+    rotateZ(random(1, 360) * 0.01);
+    rotateX(random(1, 360) * 0.01);
+    rotateY(random(1, 360) * 0.01);
+    torus(random(10, 100), 20);
     pop();
     push()
-    translate(random(-500,500), random(-200,200), random(-100,100));
-    sphere(random(10,50));
+    translate(random(-500, 500), random(-200, 200), random(-100, 100));
+    sphere(random(10, 50));
     pop();
     push();
-    translate(random(-500,500), random(-200,200), random(-100,100));
-    rotateZ(random(1,360));
-    rotateX(random(1,360) * 0.01);
-    rotateY(random(1,360) * 0.01);
-    box(random(10,70));
+    translate(random(-500, 500), random(-200, 200), random(-100, 100));
+    rotateZ(random(1, 360));
+    rotateX(random(1, 360) * 0.01);
+    rotateY(random(1, 360) * 0.01);
+    box(random(10, 70));
     pop();
     push();
-    translate(random(-500,500), random(-200,200), random(-100,100));
-    rotateZ(random(1,360) * 0.01);
-    rotateX(random(1,360) * 0.01);
-    rotateY(random(1,360) * 0.01);
-    cylinder(random(10,70));
+    translate(random(-500, 500), random(-200, 200), random(-100, 100));
+    rotateZ(random(1, 360) * 0.01);
+    rotateX(random(1, 360) * 0.01);
+    rotateY(random(1, 360) * 0.01);
+    cylinder(random(10, 70));
     pop();
     push();
-    translate(random(-500,500), random(-200,200), random(-100,100));
-    rotateZ(random(1,360) * 0.01);
-    rotateX(random(1,360) * 0.01);
-    rotateY(random(1,360) * 0.01);
-    cone(random(10,70));
+    translate(random(-500, 500), random(-200, 200), random(-100, 100));
+    rotateZ(random(1, 360) * 0.01);
+    rotateX(random(1, 360) * 0.01);
+    rotateY(random(1, 360) * 0.01);
+    cone(random(10, 70));
     pop();
   }
   //generates our space dots for the backdrop
   for (var i = 0; i < 100; i++) {
     push()
-      stroke(255)
-      strokeWeight(2)
-      ellipse(random(-1000,1000),random(-500,500),2)
+    stroke(255)
+    strokeWeight(2)
+    ellipse(random(-1000, 1000), random(-500, 500), 2)
     pop();
   }
 }
