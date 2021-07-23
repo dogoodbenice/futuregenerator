6ca0272084fb78b7e517ca2f43347bfbe6fa399c
Suraj Rai <surajrajrai@gmail.com>
2021-07-23 10:39:51 +0100

    Update sketch.js

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -30,7 +30,7 @@ function setup() {
 }
 
 function showmethefuturebutton() {
-  fbutton = createButton('🔮 '+'SHOW ME THE FUTURE  ');
+  fbutton = createButton('🔮 '+'SHOW ME THE FUTURE  '+' 🔮 ');
   fbutton.position(width/2.3, ((windowHeight) / 1.5));
   fbutton.mousePressed(setup);
 }
